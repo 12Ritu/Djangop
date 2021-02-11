bb0c9680e5198005cb0838f13cdeb927a891588d
rituayer98@gmail.com <rituayer98@gmail.com>
2021-02-11 12:05:56 +0545

    First Commit

---
+++ product_manager/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProductManagerConfig(AppConfig):
+    name = 'product_manager'
+
---
+++ templates/a.py
@@ -0,0 +1,5 @@
+ <div class="col-md-4 col-sm-4 col-lg-4">
+                <form action="search/" method="POST">
+                    <input type="text" class="form-control" placeholder="search" />
+                </form>
+            </div>
